ddb2d6361f662b78edc1045eef5bd5190c8c622d
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-12-06 09:06:17 -0600

    fix

--- src/views/public/components/Acordion.jsx
+++ src/views/public/components/Acordion.jsx
@@ -24,7 +24,7 @@ const Accordion = ({item, expandedItem, toggleAccordion}) => {
               </div>
               <p> {el.Puntaje
                 ? `Puntaje: ${el.Puntaje}`
-                : `Votos: ${el.ConteoVotos}`}
+                : `Votos: ${el.SumaCalificaciones}`}
               </p>
             </div>
           ))}
